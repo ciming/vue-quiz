f268c2875a6ee77b3d7c4b56aa0dfd2fc3eb9314
buxingxu <5211314>
2017-05-29 19:37:34 +0800

    基本显示

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="app">
     <div id="quiz">
+      <div v-for="(currentQuestion, questionNumber) in questions">
+        <div class="question">{{questionNumber+1}}、{{currentQuestion.question}}</div>
+        <div class="answer" v-for="(value, key) in currentQuestion.answers">
+          <label for="'question-'+questionNumber"><input type="radio" :name="'question-'+questionNumber"></label>
+          {{key}}. {{value}}
+        </div>
+      </div>
 
     </div>
     <div id="submit"></div>
@@ -9,20 +16,25 @@
 </template>
 
 <script>
-export default {
-  name: 'app',
-  components: {
+  import questions from './questions'
+  export default {
+    name: 'app',
+    components: {},
+    data() {
+      return {
+        questions
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ src/questions.js
@@ -0,0 +1,35 @@
+/**
+ * Created by buxingxu on 2017/5/29.
+ */
+export default  [
+  {
+    question: "工笔是哪种绘画形式的技法",
+    answers: {
+      A: "水彩画",
+      B: "油画",
+      C: "水粉画",
+      D: "国画"
+    },
+    correctAnswer: "D"
+  },
+  {
+    question: "冰激凌”是从哪国传进的外来语",
+    answers: {
+      A: "英国",
+      B: "法国",
+      C: "美国",
+      D: "俄国"
+    },
+    correctAnswer: "A"
+  },
+  {
+    question: "谬种流传”最早是宋朝人批评当时的",
+    answers: {
+      A: "官场黑暗",
+      B: "科举制度",
+      C: "社会风气",
+      D: "诗词风格"
+    },
+    correctAnswer: "B"
+  }
+];
