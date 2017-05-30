2d58749441c8611a2e66fd8567383a09b8cf09c0
buxingxu <5211314>
2017-05-30 09:00:26 +0800

    修改布局

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <div id="app">
-    <div id="quiz">
+    <div id="quiz" class="quiz-container">
       <question-item
+        class="slide"
+        :class="{active: currentSlide===questionNumber}"
         v-for="(currentQuestion, questionNumber) in questions"
         :key="questionNumber"
         :question="currentQuestion"
         :num="questionNumber"
-        ></question-item>
+        :show="showResult"
+      ></question-item>
     </div>
     <div id="submit">
-      <button type="button">提交答案</button>
+      <button id="previous" v-if="currentSlide>0" @click="currentSlide--">上一题</button>
+      <button id="next" v-if="currentSlide<questions.length-1" @click="currentSlide++">下一题</button>
+      <button type="button" @click="submit">提交答案</button>
     </div>
-    <div id="results"></div>
+    <div id="results" v-if="showResult">
+      共{{questions.length}}题，{{numCorrect}}题正确
+    </div>
   </div>
 </template>
 
@@ -23,20 +30,86 @@
     components: {
       questionItem
     },
+
     data() {
+      /**
+       * @property {Object} questions 问题数组
+       * @property {Number} currentSlide 当前问题索引
+       * @property {Boolean} showResult 是否显示答案
+       * @property {Number} numCorrect 答题错误个数
+       */
       return {
-        questions
+        questions,
+        currentSlide: 0,
+        showResult: false,
+        numCorrect: 0
+      }
+    },
+    created() {
+    },
+    methods: {
+      /**
+       * 检查错误个数
+       */
+      check() {
+        let numCorrect = 0;
+        this.questions.forEach((question, num) => {
+          if (question.select === question.correctAnswer) {
+            numCorrect++
+          }
+        })
+        this.numCorrect = numCorrect
+      },
+      submit() {
+        this.check();
+        this.showResult = true;
       }
     }
   }
 </script>
 
 <style>
+  body{
+    background-color: #f8f6f0;
+  }
   #app {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     color: #2c3e50;
     margin-top: 60px;
+  }
+
+  .slide{
+    position: absolute;
+    left: 0px;
+    top: 0px;
+    width: 100%;
+    z-index: 1;
+    opacity: 0;
+    transition: opacity 0.5s;
+  }
+  .slide.active{
+    opacity: 1;
+    z-index: 2;
+  }
+  .quiz-container{
+    position: relative;
+    height: 200px;
+    margin-top: 40px;
+  }
+  button{
+    font-family: 'Work Sans', sans-serif;
+    font-size: 16px;
+    background-color: #279;
+    color: #fff;
+    border: 0px;
+    border-radius: 3px;
+    padding: 10px;
+    cursor: pointer;
+    margin-bottom: 20px;
+  }
+  button:hover{
+    background-color: #38a;
   }
 </style>
--- src/components/question-item.vue
+++ src/components/question-item.vue
@@ -1,17 +1,63 @@
 <template>
   <div >
     <div class="question">{{num+1}}、{{question.title}}</div>
-    <div class="answer" v-for="(value, key, index) in question.answers">
-      <label :for="'answer-'+num +'-'+ index">
-        <input type="radio" :id="'answer-'+num +'-'+ index" :name="'question-'+num">{{key}}. {{value}}
+    <div class="answer"
+         v-for="(value, key, index) in question.answers"
+         :class = "{right:show&&question.select=== question.correctAnswer, error:show&&question.select!== question.correctAnswer}"
+      >
+      <label
+        :for="'answer-'+num +'-'+ index"
+        >
+        <input type="radio"
+               :id="'answer-'+num +'-'+ index"
+               :value="key"
+               :name="'question-'+num"
+               v-model="question.select"
+        >{{key}}. {{value}}
       </label>
-
     </div>
   </div>
 </template>
 
 <script>
+  import Vue from 'vue'
   export default{
-    props:['question', 'num']
+    props: {
+      question: {
+        type: Object
+      },
+      num: {
+        type: Number
+      },
+      show: {
+        type: Boolean,
+        default: false
+      }
+    },
+    created () {
+      Vue.set(this.question, 'select', '')
+    }
   }
 </script>
+
+<style scoped>
+  .question{
+    font-size: 30px;
+    margin-bottom: 10px;
+  }
+  .answers {
+    margin-bottom: 20px;
+    text-align: left;
+    display: inline-block;
+  }
+  .answers label{
+    display: block;
+    margin-bottom: 10px;
+  }
+  .answer.right{
+    color: lightgreen;
+  }
+  .answer.error{
+    color: red;
+  }
+</style>
